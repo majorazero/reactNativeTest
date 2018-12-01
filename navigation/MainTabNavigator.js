f12483db79b4950cbce92c378c898fc5b411073f
majorazero <daniel.cw.hsu@gmail.com>
2018-12-01 13:45:29 -0800

    added a esrver to play around with

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'Link',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
